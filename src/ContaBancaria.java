718d1495289e99be7aca33006d2ebe82cf4494ce
Victor Melo <contatomelo2@gmail.com>
2025-04-08 15:47:41 -0300

    criando função cheque especial, saldo, taxa

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -18,3 +18,30 @@ public class ContaBancaria {
 public double getSaldo() {
     return saldo;
 }
+
+public double getChequeEspecialDisponivel() {
+    return chequeEspecialTotal - chequeEspecialUsado;
+}
+
+public boolean estaUsandoChequeEspecial() {
+    return chequeEspecialUsado > 0;
+}
+
+public void depositar(double valor) {
+    if (chequeEspecialUsado > 0) {
+        double taxa = chequeEspecialUsado * 0.2;
+        double totalParaRepor = chequeEspecialUsado + taxa;
+
+        if (valor >= totalParaRepor) {
+            valor -= totalParaRepor;
+            System.out.printf("R$ %.2f usados para cobrir cheque especial (incluindo R$ %.2f de taxa).%n", totalParaRepor, taxa);
+            chequeEspecialUsado = 0;
+        } else {
+            chequeEspecialUsado -= (valor / 1.2);
+            System.out.printf("Depósito de R$ %.2f usado parcialmente para cobrir cheque especial.%n", valor);
+            return;
+        }
+    }
+    saldo += valor;
+    System.out.printf("Depósito realizado com sucesso! Novo saldo: R$ %.2f%n", saldo);
+}
