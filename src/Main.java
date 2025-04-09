0a54e4b5f7df0c4914ed994a78335c6f723a7b29
Victor Melo <contatomelo2@gmail.com>
2025-04-09 10:15:08 -0300

    criando opção interação sistema bancário

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Teste, incio do projeto");
+    public static void main(string[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== CRIAÇÃO DA CONTA BANCÁRIA ===");
+        System.out.println(" Informe o valor depositado na criação da conta: R$ ");
+        double valorInicial = scanner.nextDouble();
+
+        ContaBancaria conta = new ContaBancaria(valorInicial);
+        int opcao;
+
     }
 }
