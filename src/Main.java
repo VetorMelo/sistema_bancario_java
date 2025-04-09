e38f43b4ebcd5c425527f580fa35f5c12480c449
Victor Melo <contatomelo2@gmail.com>
2025-04-09 10:49:24 -0300

    Finalizando sistema bancário, iniciando teste

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,36 @@ public class Main {
             System.out.println("Escolha uma opção:  ");
             opcao = scanner.nextInt();
 
+            switch (opcao) {
+                case 1 -> System.out.printf("Saldo atual: R$ %.2f%n", conta.getSaldo());
+                case 2 -> System.out.printf("Cheque especial disponível: R$ %.2f%n", conta.getChequeEspecialDisponivel());
+                case 3 -> {
+                    System.out.print("Informe o valor do depósito: R$ ");
+                    double deposito = scanner.nextDouble();
+                    conta.depositar(deposito);
+                }
+                case 4 -> {
+                    System.out.print("Informe o valor do saque: R$ ");
+                    double saque = scanner.nextDouble();
+                    conta.sacar(saque);
+                }
+                case 5 -> {
+                    System.out.print("Informe o valor do boleto: R$ ");
+                    double boleto = scanner.nextDouble();
+                    conta.pagarBoleto(boleto);
+                }
+                case 6 -> {
+                    if (conta.estaUsandoChequeEspecial()) {
+                        System.out.println("A conta está usando o cheque especial.");
+                    }else {
+                        System.out.println("A conta NÃO está usando utilizando o cheque especial.");
+                    }
+                }
+                case 0 -> System.out.println("Encerrando o programa...");
+                default -> System.out.println("Opção inválida!");
+            }
+        } while (opcao !=0);
 
-        }
-
+        scanner.close();
     }
 }
