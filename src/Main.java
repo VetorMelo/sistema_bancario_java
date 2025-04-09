a9bcb51bc42350da6330c964ddf7284c31676826
Victor Melo <contatomelo2@gmail.com>
2025-04-09 10:30:28 -0300

    Criando opções de seleção menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(string[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== CRIAÇÃO DA CONTA BANCÁRIA ===");
@@ -10,6 +10,21 @@ public class Main {
 
         ContaBancaria conta = new ContaBancaria(valorInicial);
         int opcao;
+
+        do {
+            System.out.println("\n=== Menu ===");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Consultar cheque especial disponível");
+            System.out.println("3 - Depositar dinheiro");
+            System.out.println("4 - Sacar dinheiro");
+            System.out.println("5 - Pagar boleto");
+            System.out.println("6 - Verificar uso do cheque especial");
+            System.out.println("0 - Sair");
+            System.out.println("Escolha uma opção:  ");
+            opcao = scanner.nextInt();
+
+
+        }
 
     }
 }
